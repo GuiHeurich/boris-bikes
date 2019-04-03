137ba0e2b7f7362fb6b355e93b333838a05e85a3
Hannah Frost <h.7frost@gmail.com>
2019-04-03 15:04:08 +0100

    new bike instance made in release_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,8 @@
+require './lib/bike.rb'
 class DockingStation
 
   def release_bike
+    Bike.new
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,7 @@ require 'docking_station'
 describe DockingStation do
   #it "responds to a message" do
     it { is_expected.to respond_to :release_bike }
+    it { is_expected.to respond_to :working? }
     #expect(subject.release_bike).to eq "bike released"
   #end
 end
